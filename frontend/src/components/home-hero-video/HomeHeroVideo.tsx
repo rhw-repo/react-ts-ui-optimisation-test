2ba4146f400356985b06868dc22e9a388589eae8
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2025-11-19 14:12:25 +0000

    Sets actual WebP & Avif images for thumbnail for videoin credits. Replace placeholder (was first frame of that mp4 video). Replace original mp4 video file with compressed version & shorten file name. Refactor imports in data ts file (which are the placeholders for backend intergration) & consuming component.Refactor consuming component to remove conditional render of video vs thumbnail images, now render only images. Correct typo 'Videos' to 'Video', other videos discarded in last iteration before deployment. For testing payload size on test deploy

--- frontend/src/components/home-hero-video/HomeHeroVideo.tsx
+++ frontend/src/components/home-hero-video/HomeHeroVideo.tsx
@@ -1,5 +1,4 @@
-import droneFootage from "/assets/mp4/7226223-hd_1920_1080_30fps.mp4";
-//import droneFootage from "../../assets/mp4/Optimised7226223-hd_1920_1080_30fps.mp4";
+import droneFootage from "/assets/mp4/drone-video.mp4";
 import styles from "./HomeHeroVideo.module.css";
 
 const HomeHeroVideo: React.FC = () => {
